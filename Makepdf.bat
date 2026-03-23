7f4516ffe8312b81599f4fc420612c22d94c36a1
baquies-since1995 <139177984+baquies-since1995@users.noreply.github.com>
2026-03-23 14:52:59 -0400

    Update Makepdf.bat
    updated to new march 2026 reorg

--- Makepdf.bat
+++ Makepdf.bat
@@ -1,5 +1,29 @@
+@echo off
 REM Windows batch file to compile PDF using Pandoc
-REM Assumes Pandoc has PDF engine (e.g., via MiKTeX install)
+REM Assumes Pandoc has a PDF engine available (e.g., MiKTeX) and xelatex is installed
 
-pandoc --output=OED-Expanded.pdf --pdf-engine=xelatex --variable mainfont="Arial" --table-of-contents introduction.md core-mechanic.md characters.md equipment.md gameplay.md npcs.md rewards.md monsters.md traps.md resources.md x-additional-material.md x-campaign-material.md x-design-notes.md
-
+pandoc ^
+  --output=OED-Expanded.pdf ^
+  --pdf-engine=xelatex ^
+  --variable mainfont="Arial" ^
+  --table-of-contents ^
+  introduction.md ^
+  Players-Guide.md ^
+  core-mechanic.md ^
+  characters.md ^
+  equipment.md ^
+  gameplay.md ^
+  Judges-Guide.md ^
+  Design-a-Dungeon.md ^
+  monsters.md ^
+  rewards.md ^
+  Design-a-Settlement.md ^
+  npcs.md ^
+  Design-a-Wilderness.md ^
+  Wilderness-Exploration.md ^
+  PC-Bases-and-Strongholds.md ^
+  x-Appendix-A-Monster-Statistics.md ^
+  x-Appendix-B-The-Magic-Table.md ^
+  x-Appendix-C-Book-of-War.md ^
+  x-Appendix-D-Optional-Material.md
+  x-design-notes.md
